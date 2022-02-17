f64190803a8db66c13531263086546632e3fd575
Rob Gentry <97411164+robgentry999@users.noreply.github.com>
2022-02-17 09:54:44 -0600

    redone

--- treehouse-project-5/js/app.js
+++ treehouse-project-5/js/app.js
@@ -1,22 +1,6 @@
 //Code Snippet from
 //https://www.geeksforgeeks.org/search-bar-using-html-css-and-javascript/
 
-function search_gallery(){
-    let input = document.getElementById('search').value
-    input = input.toLowerCase();
-    let x = document.getElementsByTagName('li');
-
-    for (i = 0 ; i < x.length; i++) {
-        if(!x[i].innerHTML.toLowerCase().includes(input)) {
-            x[i].style.display = "none";
-        }
-    else {
-        x[i].style.display = "list-item";
-    }
-}
-};
-
 baguetteBox.run(".gallery");
-
 
 const search = new Filter('search', 'data-caption');
